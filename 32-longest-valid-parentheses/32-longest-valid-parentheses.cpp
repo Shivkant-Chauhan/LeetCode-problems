e181f6b0c9a5ac122f53a08f013651b248910b56
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-05-28 21:15:00 +0530

    Time: 1899 ms (5.13%), Space: 6.9 MB (87.25%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -3,24 +3,47 @@ public:
     int longestValidParentheses(string s) {
         if(s.size() == 0)   return 0;
         
-        stack<int> st;
-        st.push(-1);
+        //approach 2
+        
         int output = 0;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == ')'){
-                if(st.size() > 0){
-                    st.pop();
-                    if(st.size() == 0)  st.push(i);
-                    
-                    output = max(output, i-st.top());
-                    
-                } else{
-                    st.push(i);
+        
+        for(int j = 0; j < s.size(); j++){
+            int l = 0, r = 0;
+            for(int i = j; i < s.size(); i++){
+                if(s[i] == ')') r++;
+                else            l++;
+
+                if(r > l){
+                    l = 0;
+                    r = 0;
+                } else if(l == r){
+                    output = max(output, l*2);
                 }
-            } else{
-                st.push(i);
+                // cout <<l<<r<<" ";
             }
         }
+        
+        
+        
+        
+//         stack<int> st;
+//         st.push(-1);
+//         int output = 0;
+//         for(int i = 0; i < s.size(); i++){
+//             if(s[i] == ')'){
+//                 if(st.size() > 0){
+//                     st.pop();
+//                     if(st.size() == 0)  st.push(i);
+                    
+//                     output = max(output, i-st.top());
+                    
+//                 } else{
+//                     st.push(i);
+//                 }
+//             } else{
+//                 st.push(i);
+//             }
+//         }
         
         return output;
     }
