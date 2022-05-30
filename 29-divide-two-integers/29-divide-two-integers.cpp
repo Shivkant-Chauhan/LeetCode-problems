bbaf5efaf89e1adb9a61e5f6d42c26409583bbc6
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-05-30 11:53:26 +0530

    Time: 4 ms (49.70%), Space: 5.9 MB (30.45%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -1,10 +1,55 @@
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        long long int temp = (1LL*dividend)/divisor;
-        if(temp > INT_MAX)  return INT_MAX;
-        if(temp < INT_MIN)  return INT_MIN;
+    int divide(long long int dvd, long long int dsr) {
+        // long long int temp = (1LL*dvd)/dsr;
+        // if(temp > INT_MAX)  return INT_MAX;
+        // if(temp < INT_MIN)  return INT_MIN;
         
-        return temp;
+        if(dvd > INT_MAX) return INT_MAX;
+        if(dvd < INT_MIN) return INT_MIN;
+        
+//         return temp;
+        bool flag1 = false, flag2 = false;
+        if(dvd < 0){
+            dvd = dvd*(-1);
+            flag1 = true;
+        }
+        if(dsr < 0){
+            dsr = dsr*(-1);
+            flag2 = true;
+        }
+        
+        long long int cnt = 0;
+        while(dvd-dsr>=0){
+            long long int temp = 1;
+            long long int d1 = dvd, d2 = dsr;
+            while(d1-d2>=0){
+                cnt += temp;
+                d1 -= d2;
+                d2 *= 2;
+                temp *= 2;
+            }
+            dvd = d1;
+
+            
+        }
+
+        if(flag1 && flag2){
+            if(cnt > INT_MAX)   return INT_MAX;
+            if(cnt < INT_MIN)   return INT_MIN;
+            
+            return cnt;
+        } else if(flag1 || flag2){
+            cnt *= (-1);
+            if(cnt > INT_MAX)   return INT_MAX;
+            if(cnt < INT_MIN)   return INT_MIN;
+            
+            return cnt;
+        } else{
+            if(cnt > INT_MAX)   return INT_MAX;
+            if(cnt < INT_MIN)   return INT_MIN;
+            
+            return cnt;
+        }
     }
 };
