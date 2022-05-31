4b6aec9ff30ae556e63b3257213581d6dfde2d05
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-05-31 11:57:24 +0530

    Time: 83 ms (31.66%), Space: 7.5 MB (67.40%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -12,12 +12,12 @@ public:
         
         a += nums[start];
         bool p1 = helper(nums, start+2, end, a, b+nums[start+1]) & helper(nums, start+1, end-1, a, b+nums[end]);
-        // if(p1)  return true;
+        if(p1)  return true;
         
         a -= nums[start];
         a += nums[end];
         bool p2 = helper(nums, start+1, end-1, a, b+nums[start]) & helper(nums, start, end-2, a, b+nums[end-1]);
-        if(p1||p2)  return true;
+        if(p2)  return true;
         
         return false;
     }
