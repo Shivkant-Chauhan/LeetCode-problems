9f9a9ad50bd2019864fb6a60677c2fedd0c12ac6
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-05-31 08:43:38 +0530

    Time: 1261 ms (12.26%), Space: 97.8 MB (7.86%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+//     set<string> allK;
+    
+//     void helper(string all, int id, int k){
+//         if(id >= k)     return ;
+//         allK.insert(all);
+//         helper(all, id+1, k);
+//         all[id] = '1';
+//         allK.insert(all);
+//         helper(all, id+1, k);
+//     }
+    
+    bool hasAllCodes(string s, int k) {
+        if(s.size() <= k)   return false;
+        
+//         string all(k, '0');
+//         helper(all, 0, k);
+        
+        set<string> checker;
+        string temp = "";
+        for(int i = 0; i < k; i++){
+            temp += s[i];
+        }
+        checker.insert(temp);
+        for(int i = k; i < s.size(); i++){
+            temp = temp.substr(1);
+            temp += s[i];
+            
+            checker.insert(temp);
+        }
+        
+        // if(checker.size() == allK.size())
+        if(checker.size() == (1<<k))
+            return true;
+        else
+            return false;
+    }
+};
