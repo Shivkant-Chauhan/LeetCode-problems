cc9a066bb73b0f3a7b56138ac1a78dab21b3eb02
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-06-02 08:55:07 +0530

    Time: 10 ms (91.09%), Space: 10.8 MB (9.84%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -9,16 +9,8 @@ public:
             }
             matrix2.push_back(t);
         }
-        
-        
-        // for(int i = 0; i < matrix.size(); i++){
-        //     for(int j = i+1; j < matrix[i].size(); j++){
-        //         int t = matrix[i][j];
-        //         matrix[i][j] = matrix[j][i];
-        //         matrix[j][i] = t;
-        //     }
-        // }
-        
         return matrix2;
+        
+        
     }
 };
