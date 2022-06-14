bdbb84b7846db2602c66216de7bf10390258e6a3
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-06-14 10:12:29 +0530

    Time: 1729 ms (5.02%), Space: 384 MB (5.02%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    int helper(string word1, string word2, int**dp){
+        if(word1.size()==0 || word2.size()==0)  return max(word1.size(), word2.size());
+        
+        if(word1[0] == word2[0]){
+            return helper(word1.substr(1), word2.substr(1), dp);
+        }
+        
+        if(dp[word1.size()][word2.size()] != -1)    return dp[word1.size()][word2.size()];
+        
+        int p1 = 1+ helper(word1.substr(1), word2, dp);
+        int p2 = 1+ helper(word1, word2.substr(1), dp);
+        
+        dp[word1.size()][word2.size()] = min(p1, p2);
+        return min(p1, p2);
+    }
+    
+    int minDistance(string word1, string word2) {
+        int** dp = new int*[word1.size()+1];
+        for(int i = 0; i <= word1.size(); i++){
+            dp[i] = new int[word2.size()+1];
+            for(int j = 0; j <= word2.size(); j++)
+                dp[i][j] = -1;
+        }
+        
+        int ans = helper(word1, word2, dp);
+        
+        // for(int i = 0; i <= word1.size(); i++){
+        //     delete dp[i];
+        // }
+        // delete[] dp;
+        
+        return ans;
+    }
+};
