a0a6a0b1d37ea48cf163b28214983f7885e6e008
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-07-09 14:55:54 +0530

    Time: 15 ms (76.14%), Space: 9.2 MB (55.52%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[25][2500];
     int helper(vector<int>& nums, int n, int sum, int target){
         if(n == nums.size()){
             if(sum == target)
@@ -8,13 +9,18 @@ public:
                 return 0;
         }
         
+        if(dp[n][sum+1000] != -1){
+            return dp[n][sum+1000];
+        }
+        
         int pass1 = helper(nums, n+1, sum+nums[n], target);
         int pass2 = helper(nums, n+1, sum-nums[n], target);
         
-        return (pass1+pass2);
+        return dp[n][sum+1000] = (pass1+pass2);
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp, -1, sizeof(dp));
         return helper(nums, 0, 0, target);
     }
 };
