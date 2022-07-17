fe39a0df71b3ba2d0cdd951987c4be86e341a9a2
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-07-17 23:51:10 +0530

    Time: 363 ms (40.62%), Space: 9.4 MB (86.47%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -18,34 +18,40 @@ public:
         return dp[currSum] = p1|p2;
     }
     
-    bool canPartition(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum&1)   return false;
+//     bool canPartition(vector<int>& nums) {
+//         memset(dp, -1, sizeof(dp));
+//         int sum = accumulate(nums.begin(), nums.end(), 0);
+//         if(sum&1)   return false;
         
-        return helper(nums, sum/2, 0, 0);
-    }
+//         return helper(nums, sum/2, 0, 0);
+//     }
 
 //bottom up..!!!
-//     bool canPartition(vector<int>& nums) {
-//         int n = nums.size();
-//         int sum = accumulate(nums.begin(), nums.end(), 0);
-//         if(sum&1 || n == 1)   return false;
-//         sum /= 2;
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum&1 || n == 1)   return false;
+        sum /= 2;
         
 //         sort(nums.begin(), nums.end());
-//         vector<bool> dp(sum+1, false);
-//         dp[0] = 1;
+        vector<bool> dp(sum+1, false);
+        dp[0] = 1;
         
-//         for(int i = 0; i < n; i++){
-//             for(int j = sum; j >= 0; j--){
-//                 if(j >= nums[i]){
-//                     dp[j] = dp[j-nums[i]];
-//                 }
-//             }
+        
+//         for(int k = 1; k <= sum; k++){
+            
 //         }
+        
+        
+        for(int i = 0; i < n; i++){
+            for(int j = sum; j >= 0; j--){
+                if(j >= nums[i]){
+                    dp[j] = (dp[j] | dp[j-nums[i]]);
+                }
+            }
+        }
 
-//         return dp[sum];
-//     }
+        return dp[sum];
+    }
     
 };
