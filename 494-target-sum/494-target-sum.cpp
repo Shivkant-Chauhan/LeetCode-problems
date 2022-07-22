0284b4a921714e63c03a2afbe25cc6b70344f5d0
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-07-22 13:06:54 +0530

    Time: 26 ms (55.17%), Space: 9 MB (78.04%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+/*
         // this is simple variation of count of number of subsets with given difference
         // find the count of 2 subsets such that their difference is equsl to target!
         // and the sum of the 2 subsets is the total sum of given array
@@ -32,6 +33,39 @@ public:
             if(dp[i] > 0){
                 int x = (sumT - i);
                 if((x - i) == target)    ans += dp[i];
+            }
+        }
+        
+        return ans;
+        
+*/
+        
+        
+        int sumT = accumulate(nums.begin(), nums.end(), 0);
+        if(target > sumT)   return 0;
+        
+        int dp[nums.size()+1][sumT+1];
+        memset(dp, 0, sizeof dp);
+        for(int i = 0; i <= nums.size(); i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 1; i <= nums.size(); i++){
+            for(int j = sumT; j >= 0; j--){
+                if(j-nums[i-1] >= 0){
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                } else{
+                    dp[i][j] = dp[i-1][j];
+                }
+
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 0; i <= sumT; i++){
+            if(dp[nums.size()][i] > 0){
+                int x = sumT - i;
+                if((x - i) == target)   ans += dp[nums.size()][i];
             }
         }
         
