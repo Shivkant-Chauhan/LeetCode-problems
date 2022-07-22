f31c41da72721380df83b7d0b279bc2cae440492
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-07-22 12:55:11 +0530

    Time: 15 ms (76.42%), Space: 9.1 MB (78.04%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int dp[25][2500];
-    int helper(vector<int>& nums, int n, int sum, int target){
-        if(n == nums.size()){
-            if(sum == target)
-                return 1;
-            else
-                return 0;
-        }
-        
-        if(dp[n][sum+1000] != -1){
-            return dp[n][sum+1000];
-        }
-        
-        int pass1 = helper(nums, n+1, sum+nums[n], target);
-        int pass2 = helper(nums, n+1, sum-nums[n], target);
-        
-        return dp[n][sum+1000] = (pass1+pass2);
-    }
-    
     int findTargetSumWays(vector<int>& nums, int target) {
-        memset(dp, -1, sizeof(dp));
-        return helper(nums, 0, 0, target);
+        // this is simple variation of count of number of subsets with given difference
+        // find the count of 2 subsets such that their difference is equsl to target!
+        // and the sum of the 2 subsets is the total sum of given array
+        
+        // sum2 - sum1 = target
+        // sum2 + sum1 = totalSum
+        
+        //=> sum2 = (target+totalSum)/2
+        // find the count of the subsets having sum as sum2...
+        
+        int sumT = accumulate(nums.begin(), nums.end(), 0);
+        if(target > sumT)   return 0;
+        
+
+        int dp[sumT+1];
+        memset(dp, 0, sizeof dp);
+        dp[0] = 1;
+        
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = sumT; j >= 0; j--){
+                if(j - nums[i] >= 0){
+                    dp[j] += dp[j-nums[i]];
+                }
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 0; i <= sumT; i++){
+            if(dp[i] > 0){
+                int x = (sumT - i);
+                if((x - i) == target)    ans += dp[i];
+            }
+        }
+        
+        return ans;
     }
 };
