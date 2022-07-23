841c367c08db8cf3b0704798eefc0c5d0ff39ce9
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-07-23 22:52:22 +0530

    Time: 1455 ms (17.61%), Space: 150.5 MB (57.83%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -2,21 +2,44 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
-typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        // vector<int> ans(n, 0);
+        vector<int> ans(n, 0);
         pbds ordered_set;
         
         for(int i = n-1; i >= 0; i--){
-            int x = ordered_set.order_of_key({nums[i], -1});
-            ordered_set.insert({nums[i], i});
-            nums[i] = x;
+            ans[i] = ordered_set.order_of_key(nums[i]);
+            ordered_set.insert(nums[i]);
         }
         
-        return nums;
+        return ans;
     }
 };
+
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+// class Solution {
+// public:
+//     vector<int> countSmaller(vector<int>& nums) {
+//         int n = nums.size();
+//         // vector<int> ans(n, 0);
+//         pbds ordered_set;
+        
+//         for(int i = n-1; i >= 0; i--){
+//             int x = ordered_set.order_of_key({nums[i], -1});
+//             ordered_set.insert({nums[i], i});
+//             nums[i] = x;
+//         }
+        
+//         return nums;
+//     }
+// };
