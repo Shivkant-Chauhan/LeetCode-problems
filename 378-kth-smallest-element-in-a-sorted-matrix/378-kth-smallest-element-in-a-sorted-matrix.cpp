211095f09f4e1bcace1675620e319abfdb8289f6
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-08-02 10:18:36 +0530

    Time: 44 ms (71.90%), Space: 14.2 MB (45.52%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -5,16 +5,17 @@ public:
         int m = matrix.size(), n = matrix[0].size();
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(k){
-                    pq.push(matrix[i][j]);
-                    k--;
-                } else{
-                    if(pq.top() > matrix[i][j]){
-                        pq.pop();
-                        pq.push(matrix[i][j]);
-                    }
-                }
-                
+                // if(k){
+                //     pq.push(matrix[i][j]);
+                //     k--;
+                // } else{
+                //     if(pq.top() > matrix[i][j]){
+                //         pq.pop();
+                //         pq.push(matrix[i][j]);
+                //     }
+                // }
+                pq.push(matrix[i][j]);
+                if(pq.size() > k)   pq.pop();
             }
         }
         return pq.top();
