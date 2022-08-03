4164aa784b7877ce011e0d6305fe28991f09c629
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-08-03 11:36:56 +0530

    Time: 189 ms (39.12%), Space: 38.7 MB (68.90%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,20 +1,40 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, //put greater in place of less for storing ordered set in desc order
+        rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef tree<int, null_type, greater<int>, //put greater in place of less for storing ordered set in desc order
+        rb_tree_tag, tree_order_statistics_node_update> pbds2;
+
+
 class MyCalendar {
 public:
     vector<int> st, ed;
+    map<int, int> mp;
+    
     MyCalendar() {
-        
     }
     
     bool book(int start, int end) {
-        for(int i = 0; i < st.size(); i++){
-            if(start > st[i] && start < ed[i])  return false;
-            if(end > st[i] && end < ed[i])      return false;
-            if(start <= st[i] && end >= ed[i])    return false;
-        }
+//         for(int i = 0; i < st.size(); i++){
+//             if(start > st[i] && start < ed[i])  return false;
+//             if(end > st[i] && end < ed[i])      return false;
+//             if(start <= st[i] && end >= ed[i])    return false;
+//         }
         
-        st.push_back(start);
-        ed.push_back(end);
-        return true;
+//         st.push_back(start);
+//         ed.push_back(end);
+//         return true;
+        
+        
+        auto it=mp.upper_bound(start);
+        if(it==mp.end() || it->second>=end){
+             mp[end]=start;
+             return true;
+        } else{
+            return false;
+        }
     }
 };
 
