a697ea45efd04667c681e4aae436f120e4ed8ec6
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-08-06 19:46:51 +0530

    Time: 8 ms (33.86%), Space: 6.8 MB (55.13%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -5,18 +5,38 @@ public:
         string output = "";
         if(!n)  return output;
         
-        stack<char> st;
-        st.push(s[0]);
-        for(int i = 1; i < n; i++){
-            if(s[i] == '('){
-                if(st.size() > 0)
-                    output += s[i];
-                st.push(s[i]);
-            } else{
-                st.pop();
-                if(!st.empty()) output += s[i];
+        
+        //easier approach
+        int i = 0, j = 0;
+        int l = 0, r = 0;
+        while(i < n){
+            while(true){
+                if(s[j] == '(') l++;
+                else            r++;
+                if(l == r)  break;
+                j++;
             }
+
+            output += s.substr(i+1, j-i-1);
+            i = j+1;
+            j++;
         }
+        
+        
+            
+        
+        // stack<char> st;
+        // st.push(s[0]);
+        // for(int i = 1; i < n; i++){
+        //     if(s[i] == '('){
+        //         if(st.size() > 0)
+        //             output += s[i];
+        //         st.push(s[i]);
+        //     } else{
+        //         st.pop();
+        //         if(!st.empty()) output += s[i];
+        //     }
+        // }
         
         return output;
     }
