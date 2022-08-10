f6bdf370a5cdde4c09088955db02bb6dabef0560
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-08-10 10:43:22 +0530

    Time: 200 ms (9.02%), Space: 8.5 MB (42.94%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char, int> mp;
-        int ans = 0, prev = 0;
-        for(int i=0;i<s.size();i++) {
-            ans = max(ans, i-prev);
-            if(mp.find(s[i])!=mp.end() && mp[s[i]]>=prev) {
-                prev = mp[s[i]] + 1;
-            }
-            mp[s[i]] = i;
-        }
+//         map<char, int> mp;
+//         int ans = 0, prev = 0;
+//         for(int i=0;i<s.size();i++) {
+//             ans = max(ans, i-prev);
+//             if(mp.find(s[i])!=mp.end() && mp[s[i]]>=prev) {
+//                 prev = mp[s[i]] + 1;
+//             }
+//             mp[s[i]] = i;
+//         }
 		
-        if(ans < s.size() - prev)
-            return s.size() - prev;
+//         if(ans < s.size() - prev)
+//             return s.size() - prev;
+        
+//         return ans;
+        map<char, int> mp;
+        int ans = 0, j = 0;
+        for(int i = 0; i < s.size(); i++){
+            cout << mp[s[i]];
+            if(mp[s[i]] > 0){
+                while(j < s.size() && mp[s[i]] > 0){
+                    mp[s[j++]]--;
+                }
+            }
+            mp[s[i]]++;
+            ans = max(ans, i-j+1);
+            cout << i-j+1 << ". ";
+        }
         
         return ans;
     }
