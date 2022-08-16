d82ade9dfe7003adc8e2689c343a0879b0fe55c5
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-08-16 08:15:05 +0530

    Time: 84 ms (24.99%), Space: 10.9 MB (15.65%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char, int> hsh;
+        for(int i = 0; i < s.size(); i++){
+            hsh[s[i]]++;
+        }
+        
+        for(int i = 0; i < s.size(); i++)
+            if(hsh[s[i]] == 1)  return i;
+        
+        return -1;
+    }
+};
